1ec9f206434626e2e70b755eea9bcb58d5379bd1
webner <webner@webner-pc>
2018-10-12 14:38:48 +0530

    Another code

--- src/test/SampleCode.java
+++ src/test/SampleCode.java
@@ -1,20 +1,31 @@
 package test;
 
+//import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SampleCode {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","E:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		
-		WebDriver c = new ChromeDriver();
-		c.get("https://widget-test-site.s3.amazonaws.com/THEMES_TEST/tac_button_theme_test.html");
-		c.manage().window().maximize();
 		
-c.findElement(By.xpath("//img[@src='//dev.turnacar-content.redlineinventory.com/player_themes/lib/lexus-00/open_turnacar.png']")).getCssValue("hello");
-	}
+		WebDriver c = new ChromeDriver();
+		c.manage().window().maximize();
+		c.get("https://www.proposalways.com/userLogin");
+		
+		c.findElement(By.id("user_email")).sendKeys("naman.gupta@webners.com");
+		c.findElement(By.id("user_email")).sendKeys(Keys.CONTROL+"ac");
+		c.findElement(By.id("user_email")).clear();
+		c.findElement(By.id("user_password")).sendKeys(Keys.CONTROL+"av");
+		c.findElement(By.id("loginButton")).submit();
 
+	}
 }
+		
+		
+
