38d09243e72721a8e4d0c6302e5ebf33c8b3e6d5
webner <webner@webner-pc>
2018-10-16 16:52:28 +0530

    Get all and click all the tabs of templates

--- src/test/SampleCode.java
+++ src/test/SampleCode.java
@@ -1,11 +1,16 @@
 package test;
 
+import org.openqa.selenium.Dimension;
+
+import java.util.List;
+
 //import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class SampleCode {
 
@@ -17,15 +22,26 @@ public class SampleCode {
 		WebDriver c = new ChromeDriver();
 		c.manage().window().maximize();
 		c.get("https://www.proposalways.com/userLogin");
-		
 		c.findElement(By.id("user_email")).sendKeys("naman.gupta@webners.com");
-		c.findElement(By.id("user_email")).sendKeys(Keys.CONTROL+"ac");
-		c.findElement(By.id("user_email")).clear();
-		c.findElement(By.id("user_password")).sendKeys(Keys.CONTROL+"av");
-		c.findElement(By.id("loginButton")).submit();
-
+		c.findElement(By.id("user_password")).sendKeys("abc123");
+		c.findElement(By.id("loginButton")).click();
+		Thread.sleep(5000);
+		c.findElement(By.id("arrowsidenavbar")).click();
+		
+		Actions d = new Actions(c);
+		d.moveToElement(c.findElement(By.linkText("Templates"))).build().perform();
+		Thread.sleep(2000);
+		c.findElement(By.linkText("View")).click();
+		
+		Thread.sleep(2000);
+		
+		List <WebElement> tabs=c.findElements(By.xpath("//ul[contains(@class,'nav-justified')]//li"));
+		for(int i=0;i<tabs.size();i++)
+		{
+			Thread.sleep(2000);
+			System.out.println(tabs.get(i).getText());
+		}
 	}
 }
-		
 		
 
